e631515deea725093cabad7fddd1cf59d1eb969b
Luis Marte <lmarte17@gmail.com>
2016-09-27 15:30:21 +0000

    Methods Evercises

---
+++ main.js
@@ -0,0 +1,3 @@
+var answer = prompt("Enter your data. NOW!");
+
+alert("You entered: " + answer + "\nThere are " + answer.length + " characters in the word.\nThe third character is '" + answer.charAt(2) + "'.\nLowercase: " + answer.toLowerCase() + "\nUppercase: " + answer.toUpperCase() + "\nThere once was a " + answer + " over yonder.\nSubword: " + answer.substring(1,4));
